7b07fc6ff00ee7a282938f2d7479dbc7820a9b2e
shafin <shafinshahriar340@gmail.com>
2021-11-09 23:58:47 +0600

    navbar  css removed, styled added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,66 @@
 import React from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { AiOutlineBars } from 'react-icons/ai';
+import styled from 'styled-components';
 import Logo from '../../assets/images/logo.svg';
-import './Navbar.css';
 
 const BootNavbar = () => {
 	return (
 		<>
-			<Navbar expand='lg' className='sticky-top bg-nav-dark'>
-				<Container>
-					<Navbar.Brand href='/'>
-						<img className='brand-logo' src={Logo} alt='' />
-					</Navbar.Brand>
+			<NavWrapper>
+				<Navbar expand='lg'>
+					<Container>
+						<Navbar.Brand href='/'>
+							<BrandLogo src={Logo} alt='' />
+						</Navbar.Brand>
 
-					<Navbar.Toggle aria-controls='basic-navbar-nav'>
-						<AiOutlineBars style={{ color: '#fff', fontSize: '28px' }} />
-					</Navbar.Toggle>
+						<Navbar.Toggle aria-controls='basic-navbar-nav'>
+							<AiOutlineBars style={{ color: '#fff', fontSize: '28px' }} />
+						</Navbar.Toggle>
 
-					<Navbar.Collapse id='basic-navbar-nav'>
-						<Nav className='navbar-nav mx-auto mb-2 mb-lg-0'>
-							<Nav.Link href='/' className='nav-link is-white-hov-blue'>
-								Home
+						<Navbar.Collapse id='basic-navbar-nav'>
+							<Nav className='navbar-nav mx-auto mb-2 mb-lg-0'>
+								<NavLink href='/' className='nav-link'>
+									Home
+								</NavLink>
+								<NavLink href='#services' className='nav-link'>
+									Services
+								</NavLink>
+								<NavLink href='#pricing' className='nav-link'>
+									Pricing
+								</NavLink>
+							</Nav>
+
+							<Nav.Link className='p-0' href='#newsletter'>
+								<Button className='main-btn'>CONTACT US</Button>
 							</Nav.Link>
-							<Nav.Link href='#services' className='nav-link is-white-hov-blue'>
-								Services
-							</Nav.Link>
-							<Nav.Link href='#pricing' className='nav-link is-white-hov-blue'>
-								Pricing
-							</Nav.Link>
-						</Nav>
-						<Nav.Link className='p-0' href='#pricing'>
-							<Button className='main-btn'>CONTACT US</Button>
-						</Nav.Link>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
+						</Navbar.Collapse>
+					</Container>
+				</Navbar>
+			</NavWrapper>
 		</>
 	);
 };
 
 export default BootNavbar;
+
+const NavWrapper = styled.div`
+	background-color: #182432;
+	position: sticky;
+	top: 0;
+	z-index: 1020;
+`;
+const BrandLogo = styled.img`
+	height: 2.25rem;
+	-webkit-user-drag: none;
+	user-select: none;
+	-webkit-user-select: none;
+	-ms-user-select: none;
+`;
+const NavLink = styled.a`
+	font-size: 16px;
+	color: #fff !important;
+	&:hover {
+		color: rgb(4, 177, 252) !important;
+	}
+`;
