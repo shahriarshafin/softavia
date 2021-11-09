e773bdee0956d4f1cf011c53f1d0f6bddaa102f0
shafin <shafinshahriar340@gmail.com>
2021-11-10 00:08:25 +0600

    hero css removed, styled added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,25 @@
 import '@lottiefiles/lottie-player';
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import './Hero.css';
+import styled from 'styled-components';
 const Hero = () => {
 	return (
 		<>
-			<section className='hero'>
+			<section>
 				<Container>
 					<Row>
 						<Col xs={12} lg={6}>
-							<div className='hero-wrapper'>
-								<h1 className='hero-heading'>
+							<HeroWrapper>
+								<HeroHeading>
 									Make Your Business
 									<span className='main-gradient'> More Powerfull</span>
-								</h1>
-								<h2 className='hero-description'>
+								</HeroHeading>
+								<HeroDescription>
 									We have been on this journey for the last 3 years reaching the
 									satisfaction level of our clients through developing equitable
 									software as per requirements.
-								</h2>
-							</div>
+								</HeroDescription>
+							</HeroWrapper>
 							<button type='button' className='main-btn'>
 								<a className='text-decoration-none text-light' href='#pricing'>
 									ORDER NOW
@@ -47,3 +47,28 @@ const Hero = () => {
 };
 
 export default Hero;
+
+const HeroWrapper = styled.div`
+	margin-top: 4rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+`;
+const HeroHeading = styled.h1`
+	color: #fff;
+	font-size: 3rem;
+	line-height: 1;
+	font-weight: 600;
+	letter-spacing: 0.025em;
+`;
+const HeroDescription = styled.h2`
+	color: #9b9ea2;
+	font-size: 1.25rem;
+	line-height: 1.75rem;
+	font-weight: 400;
+	line-height: 2.5rem;
+	letter-spacing: 0.025em;
+	margin-top: 1rem;
+	margin-bottom: 1rem;
+`;
