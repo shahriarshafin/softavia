3a7d4e5ab7ef8ba1df11405a249bc2d5e504b479
shafin <shafinshahriar340@gmail.com>
2021-11-11 05:25:00 +0600

    feature css removed, styled added

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { IconContext } from 'react-icons';
 import {
 	AiOutlineAppstore,
 	AiOutlineBarChart,
@@ -9,13 +8,13 @@ import {
 	AiOutlineRadarChart,
 	AiOutlineRocket,
 } from 'react-icons/ai';
+import styled from 'styled-components';
 import MobileMoc from '../../assets/images/mobile.png';
-import './Features.css';
 
 const Features = () => {
 	return (
 		<>
-			<section className='feature' id='services'>
+			<section id='services'>
 				<Container>
 					<h2 className='feature-heading mt-5'>
 						Why our product is <span className='main-gradient'>the best</span>
@@ -25,123 +24,117 @@ const Features = () => {
 						product.
 					</p>
 
-					<Row className='feature-item-wrapper'>
+					<FeatureItemWrap className='row'>
 						<Col xs={12} lg={4}>
 							<Row>
 								<Col xs={12}>
-									<div className='feature-item'>
-										<IconContext.Provider value={{ className: 'icon' }}>
-											<div>
-												<AiOutlineNodeExpand />
-											</div>
-										</IconContext.Provider>
+									<FeatureItem>
+										<Icons>
+											<AiOutlineNodeExpand />
+										</Icons>
+
 										<div className='ms-4'>
-											<h6 className='heading'>Built for Developers</h6>
-											<p className='feature-item-description'>
+											<h6>Built for Developers</h6>
+											<p>
 												Material App is built to make your life easier. Theme
 												provider, build tooling, documentation, and 400.
 											</p>
 										</div>
-									</div>
+									</FeatureItem>
 								</Col>
 
 								<Col xs={12}>
-									<div className='feature-item'>
-										<IconContext.Provider value={{ className: 'icon' }}>
-											<div>
-												<AiOutlineAppstore />
-											</div>
-										</IconContext.Provider>
+									<FeatureItem>
+										<Icons>
+											<AiOutlineAppstore />
+										</Icons>
+
 										<div className='ms-4'>
-											<h6 className='heading'>Fast Prototyping</h6>
-											<p className='feature-item-description'>
+											<h6>Fast Prototyping</h6>
+											<p>
 												Material App is built to make your life easier. Theme
 												provider, build tooling, documentation, and 400.
 											</p>
 										</div>
-									</div>
+									</FeatureItem>
 								</Col>
 
 								<Col xs={12}>
-									<div className='feature-item'>
-										<IconContext.Provider value={{ className: 'icon' }}>
-											<div>
-												<AiOutlineRocket />
-											</div>
-										</IconContext.Provider>
+									<FeatureItem>
+										<Icons>
+											<AiOutlineRocket />
+										</Icons>
+
 										<div className='ms-4'>
-											<h6 className='heading'>Verified Users</h6>
-											<p className='feature-item-description'>
+											<h6>Verified Users</h6>
+											<p>
 												Material App is built to make your life easier. Theme
 												provider, build tooling, documentation, and 400.
 											</p>
 										</div>
-									</div>
+									</FeatureItem>
 								</Col>
 							</Row>
 						</Col>
 
 						<Col xs={12} lg={4} className='d-flex justify-content-center'>
-							<div className='img-wrapper'>
+							<ImageWrap>
 								<img src={MobileMoc} height='' alt='' />
-							</div>
+							</ImageWrap>
 						</Col>
 
 						<Col xs={12} lg={4}>
 							<Row>
 								<Col xs={12}>
-									<div className='feature-item'>
-										<IconContext.Provider value={{ className: 'icon' }}>
-											<div>
-												<AiOutlineBarChart />
-											</div>
-										</IconContext.Provider>
+									<FeatureItem>
+										<Icons>
+											<AiOutlineBarChart />
+										</Icons>
+
 										<div className='ms-4'>
-											<h6 className='heading'>Collaborate</h6>
-											<p className='feature-item-description'>
+											<h6>Collaborate</h6>
+											<p>
 												Material App is built to make your life easier. Theme
 												provider, build tooling, documentation, and 400.
 											</p>
 										</div>
-									</div>
+									</FeatureItem>
 								</Col>
 
 								<Col xs={12}>
-									<div className='feature-item'>
-										<IconContext.Provider value={{ className: 'icon' }}>
-											<div>
-												<AiOutlineDashboard />
-											</div>
-										</IconContext.Provider>
+									<FeatureItem>
+										<Icons>
+											<AiOutlineDashboard />
+										</Icons>
+
 										<div className='ms-4'>
-											<h6 className='heading'>Location Integrated</h6>
-											<p className='feature-item-description'>
+											<h6>Location Integrated</h6>
+											<p>
 												Material App is built to make your life easier. Theme
 												provider, build tooling, documentation, and 400.
 											</p>
 										</div>
-									</div>
+									</FeatureItem>
 								</Col>
 
 								<Col xs={12}>
-									<div className='feature-item'>
-										<IconContext.Provider value={{ className: 'icon' }}>
-											<div>
-												<AiOutlineRadarChart />
-											</div>
-										</IconContext.Provider>
+									<FeatureItem>
+										<Icons>
+											<AiOutlineRadarChart />
+										</Icons>
+
 										<div className='ms-4'>
-											<h6 className='heading'>Easy to Use</h6>
-											<p className='feature-item-description'>
+											<h6>Easy to Use</h6>
+											<p>
 												Material App is built to make your life easier. Theme
 												provider, build tooling, documentation, and 400.
 											</p>
 										</div>
-									</div>
+									</FeatureItem>
 								</Col>
 							</Row>
 						</Col>
-					</Row>
+					</FeatureItemWrap>
 				</Container>
 			</section>
 		</>
@@ -149,3 +142,42 @@ const Features = () => {
 };
 
 export default Features;
+
+const ImageWrap = styled.div`
+	max-width: 260px;
+	img {
+		-webkit-user-drag: none;
+		user-select: none;
+		max-width: 100%;
+		height: auto;
+	}
+`;
+const FeatureItemWrap = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+const FeatureItem = styled.div`
+	display: flex;
+	text-align: left;
+	padding: 18px 20px;
+
+	h6 {
+		color: #fff;
+		font-size: 1.125rem;
+		font-weight: 500;
+		line-height: 1.25;
+	}
+	p {
+		color: #9b9ea2;
+		font-size: 0.9rem;
+		font-weight: 400;
+		line-height: 1.75;
+	}
+`;
+const Icons = styled.div`
+	svg {
+		font-size: 35px;
+		color: #54afe6;
+	}
+`;
